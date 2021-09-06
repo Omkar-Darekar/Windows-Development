add86ae0afe95ea97c8be14672359f21ad958e21
Omkar Ashok Darekar <32523193+Omkar-Darekar@users.noreply.github.com>
2021-09-06 23:57:54 +0530

    Class Factory With Reg File
    This is a class factory concept implementation in COM.
    Server and client code included. Also README file included in client part to install/paste DLL in SysWow64.

--- 02-COM/01-ClassFactoryWithRegFile/01-Server/ClassFactoryDllServerWithRegFile.cpp
+++ 02-COM/01-ClassFactoryWithRegFile/01-Server/ClassFactoryDllServerWithRegFile.cpp
@@ -1,4 +1,8 @@
-#include <Windows.h>
+/*
+* cl.exe /c /EHsc ClassFactoryDllServerWithRegFile.cpp
+* link.exe ClassFactoryDllServerWithRegFile.obj /DLL /DEF:ClassFactoryDllServerWithRegFile.def user32.lib gdi32.lib ole32.lib oleaut32.lib /SUBSYSTEM:WINDOWS
+*/
+#include <windows.h>
 #include "ClassFactoryDllServerWithRegFile.h"
 
 //class declaration 
@@ -9,7 +13,7 @@ public:
 	CSumSubtract();
 	~CSumSubtract();
 
-	HRESULT __stdcall SumOfTwoNumbers(int, int, int*);
+	HRESULT __stdcall SumOfTwoIntegers(int, int, int*);
 	HRESULT __stdcall SubtractionOfTwoIntegers(int, int, int*);
 
 	HRESULT __stdcall QueryInterface(REFIID, void**);
@@ -18,7 +22,7 @@ public:
 
 };
 
-class CSumCubtractClassFactory : public IClassFactory {
+class CSumSubtractClassFactory : public IClassFactory {
 private:
 	long m_cRef;
 public:
@@ -28,7 +32,7 @@ public:
 	HRESULT __stdcall CreateInstance(IUnknown*, REFIID, void**);
 	HRESULT __stdcall LockServer(BOOL);
 
-	HRESULT __stdcall QueryResult(REFIID, void**);
+	HRESULT __stdcall QueryInterface(REFIID, void**);
 	ULONG __stdcall AddRef(void);
 	ULONG __stdcall Release(void);
 };
@@ -59,7 +63,137 @@ CSumSubtract::~CSumSubtract(void) {
 	InterlockedDecrement(&glNumberOfActiveComponents);
 }
 
-HRESULT CSumSubtract::SumOfTwoNumbers(int num1, int num2, int* pSum) {
+HRESULT CSumSubtract::SumOfTwoIntegers(int num1, int num2, int* pSum) {
 	*pSum = num1 + num2;
 	return(S_OK);
+}
+
+HRESULT CSumSubtract::SubtractionOfTwoIntegers(int num1, int num2, int* pSubtract) {
+	*pSubtract = num1 - num2;
+	return (S_OK);
+}
+
+HRESULT CSumSubtract::QueryInterface(REFIID riid, void** ppv) {
+	if (riid == IID_IUnknown) {
+		*ppv = static_cast<ISum*>(this);
+	}
+	else if (riid == IID_ISum) {
+		*ppv = static_cast<ISum*>(this);
+	}
+	else if (riid == IID_ISubtract) {
+		*ppv = static_cast<ISubtract*>(this);
+	}
+	else {
+		*ppv = NULL;
+		return (E_NOINTERFACE);
+	}
+	reinterpret_cast<IUnknown*>(*ppv)->AddRef();
+	return (S_OK);
+}
+
+ULONG CSumSubtract::AddRef(void) {
+	InterlockedIncrement(&m_cRef);
+	return (m_cRef);
+}
+
+ULONG CSumSubtract::Release(void) {
+	InterlockedDecrement(&m_cRef);
+	if (m_cRef == 0) {
+		delete(this);
+		return 0;
+	}
+	return(m_cRef);
+}
+
+CSumSubtractClassFactory::CSumSubtractClassFactory(void) {
+	m_cRef = 1;
+}
+
+CSumSubtractClassFactory::~CSumSubtractClassFactory(void) {
+
+}
+
+HRESULT CSumSubtractClassFactory::CreateInstance(IUnknown* pUnkOuter, REFIID riid, void** ppv) {
+	CSumSubtract* pCSumSubtract = NULL;
+	HRESULT hr;
+
+	if (pUnkOuter != NULL) {
+		return (CLASS_E_NOAGGREGATION);
+	}
+
+	pCSumSubtract = new CSumSubtract;
+	if (pCSumSubtract == NULL) {
+		return (E_OUTOFMEMORY);
+	}
+
+	hr = pCSumSubtract->QueryInterface(riid, ppv);
+	pCSumSubtract->Release();
+	return (hr);
+}
+
+HRESULT CSumSubtractClassFactory::LockServer(BOOL fLock) {
+	if (fLock) {
+		InterlockedIncrement(&glNumberOfServerLocks);
+	}
+	else {
+		InterlockedDecrement(&glNumberOfServerLocks);
+	}
+	return (S_OK);
+}
+
+HRESULT CSumSubtractClassFactory::QueryInterface(REFIID riid, void** ppv) {
+	if (riid == IID_IUnknown) {
+		*ppv = static_cast<IClassFactory*>(this);
+	}
+	else if (riid == IID_IClassFactory) {
+		*ppv = static_cast<IClassFactory*>(this);
+	}
+	else {
+		*ppv = NULL;
+		return (E_NOINTERFACE);
+	}
+	reinterpret_cast<IUnknown*>(*ppv)->AddRef();
+	return(S_OK);
+}
+
+ULONG CSumSubtractClassFactory::AddRef(void) {
+	InterlockedIncrement(&m_cRef);
+	return (m_cRef);
+}
+
+ULONG CSumSubtractClassFactory::Release(void) {
+	InterlockedDecrement(&m_cRef);
+	if (m_cRef == 0) {
+		delete(this);
+		return (0);
+	}
+	return(m_cRef);
+}
+
+HRESULT __stdcall DllGetClassObject(REFCLSID rclsid, REFIID riid, void** ppv) {
+	CSumSubtractClassFactory* pCSumSubtractClassFactory = NULL;
+	HRESULT hr;
+
+	if (rclsid != CLSID_SumSubtract) {
+		return (CLASS_E_CLASSNOTAVAILABLE);
+	}
+
+	pCSumSubtractClassFactory = new CSumSubtractClassFactory;
+	if (pCSumSubtractClassFactory == NULL) {
+		return (E_OUTOFMEMORY);
+	}
+
+	hr = pCSumSubtractClassFactory->QueryInterface(riid, ppv);
+	pCSumSubtractClassFactory->Release();
+
+	return (hr);
+}
+
+HRESULT __stdcall DllCanUnloadNow(void) {
+	if ((glNumberOfActiveComponents == 0) && (glNumberOfServerLocks == 0)) {
+		return (S_OK);
+	}
+	else {
+		return (S_FALSE);
+	}
 }
---
+++ 02-COM/01-ClassFactoryWithRegFile/02-Client/README.txt
@@ -0,0 +1,5 @@
+Steps to copy DLL - 
+1. Open "ClientFactory.reg".
+2. Copy and paste "ClassFactoryDllServerWithRegFile.dll" to the 5th line of "ClassFactory.reg" file mentioned path.
+3. After pasting file, double click on "ClassFactory.reg" file and click Next.
+4. Now, run client exe.
